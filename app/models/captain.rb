5358cc49f3ab12cad990d42201c6c4f9c46f5e4d
yukijina <yukijina@gmail.com>
2019-05-04 16:52:44 -0700

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #nested joings boats => classifications
+    self.joins(boats: :classifications).group('boats.id').where('classifications.name = "Catamaran"')
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).group('boats.id').where('classifications.name = "Sailboat"').uniq
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: :classifications).group('boats.id').having('classifications.name': "Motorboat" && "Sailboat").sort
+  end
+
+  def self.non_sailors
+    self.joins(boats: :classifications).group('boats.id').having('classifications.name = "Sailboat"')
+  end
 end
